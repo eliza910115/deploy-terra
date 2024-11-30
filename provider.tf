b476a4ea033d92a496267e66e64fbbaa71de9055
Elizabet Blandon <elizabet.blandon@samtel.eu>
2024-11-30 09:55:06 -0500

    primer despliegue actions

--- provider.tf
+++ provider.tf
@@ -5,26 +5,22 @@ terraform {
       version = "~> 5.0"
     }
   }
-
+  # State-data-file storage: type and path. S3 bucket and DynamoDB table must be exist on AWS Account
   backend "s3" {
-
-    bucket = "myawsbucketdavego"
-    key = "itmLab/test.tfstate"
-    encrypt = true
-    region = "us-east-1"
-    shared_credentials_files = [ "C:/Users/anderson.velasquez/.aws/credentials" ]
-    profile = "default"
-    dynamodb_table = "tfstate_rne"
-    
+    bucket                      = "terraform-tfstates-deploy"
+    key                         = "rne/rne.tfstate"
+    encrypt                     = true
+    region                      = "us-east-1"
+    shared_credentials_files    = ["C:/Users/elizabet.blandon/.aws/credentials"]
+    profile                     = "default"
+    dynamodb_table              = "tfstate_deploy"
   }
-
-  
 }
-
+ 
 # AWS Provider
 provider "aws" {
-  #shared_config_files      = ["C:/Users/anderson.velasquez/.aws/config"]
-  #shared_credentials_files = ["C:/Users/anderson.velasquez/.aws/credentials"]
+  shared_config_files      = ["C:/Users/elizabet.blandon/.aws/config"]
+  shared_credentials_files = ["C:/Users/elizabet.blandon/.aws/credentials"]
   profile                  = "default"
   region                   = "us-east-1"
 }
